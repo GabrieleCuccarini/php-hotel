f5f8c9cd7ea1bcd6c7e867aafe7d7e15839b36c8
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-12-20 18:00:24 +0100

    Base

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+// $teamsList= ["Argentina", "Portogallo", "Francia", "Marocco", "Brasile"];
+
+// $teams = [
+// ];
+
+// foreach ($teamsList as $teamName) {
+//     $teams[] = [
+//         "name" => $teamName,
+//         "falli_subiti" => rand(1,200),
+//         "gol_fatti" => rand(1,200),
+//         "cartellini_gialli" =>rand(1,100),
+//     ];
+// }
+
+// print_r($teams);
+
+?>
